83564a3074bd4be50185ea5079a9f30c8e4270d0
Mohamed Khalid <scii220011@development.local>
2025-09-15 12:43:51 +0530

    Backend Architecture for Code Karaoke

---
+++ src/main/java/com/scii/codeKaraoke/CodeKaraokeApplication.java
@@ -0,0 +1,13 @@
+package com.scii.codeKaraoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeKaraokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeKaraokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scii/codeKaraoke/CodeKaraokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scii.codeKaraoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeKaraokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
